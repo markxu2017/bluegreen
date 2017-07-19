0fc1208e5cb8476478b4a001a3c7de38200c151f
markxu2017 <30281300+markxu2017@users.noreply.github.com>
2017-07-19 16:21:53 -0500

    Update image.php

--- image.php
+++ image.php
@@ -5,13 +5,13 @@ $white = imagecolorallocate($im, 255, 255, 255);
 
 $deployment = getenv("COLOR");
 
-if ( $deployment == 'blue') {
+//if ( $deployment == 'blue') {
 //  $color = imagecolorallocate($im, 0, 0, 255);
 //} elseif ($deployment == 'green')  {
-  $color = imagecolorallocate($im, 0, 155, 0);
-} else {
+//  $color = imagecolorallocate($im, 0, 155, 0);
+//} else {
   $color = imagecolorallocate($im, 0, 0, 255);
-}
+//}
 
 // Draw a filled rectangle
 imagefilledrectangle($im, 0, 0, 199, 199, $color);
